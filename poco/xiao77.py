6ec6dceaa86191cabd1e5bdb14d61ef972a307f7
Lei <516292026@qq.com>
2019-03-22 12:07:37 +0800

    修改 下载图片设置缓存

--- poco/xiao77.py
+++ poco/xiao77.py
@@ -20,7 +20,7 @@ headers = {
     "Accept-Language": "zh-CN,zh;q=0.9",
     "Cache-Control": "max-age=0",
     "Connection": "keep-alive",
-    # "Cookie": "4bd54_ol_offset=166646; 4bd54_ipstate=1550729151; 4bd54_readlog=%2C1944825%2C; 4bd54_c_stamp=1550729821; 4bd54_lastpos=F6; 4bd54_lastvisit=674%091550729821%09%2Fbbs%2Fthread.php%3Ffid6; sc_is_visitor_unique=rx4629288.1550729958.D340E62FE57B4FF097095612EED92D8C.1.1.1.1.1.1.1.1.1",
+    # "Cookie": "4bd54_ol_offset=166646; 4bd54_ipstate=1550729151; 4bd54_readlog=%2C1944825%2C; 4bd54_c_stamp=1553222087; 4bd54_lastpos=other; 4bd54_lastvisit=1%091553222087%09%2Fbbs%2Fhitcache.php%3Ftid1951796; sc_is_visitor_unique=rx4629288.1553222259.BF25414F54224FF7D85B1CAACD708BCF.1.1.1.1.1.1.1.1.1",
     "Host": "x771117.net",
     "Referer": "http://x77556.net/bbs/",
     "Upgrade-Insecure-Requests": "1",
@@ -37,23 +37,27 @@ cookie_jar = RequestsCookieJar()
 # cookie_jar.set("4bd54_lastvisit","674%091550729821%09%2Fbbs%2Fthread.php%3Ffid6")
 # cookie_jar.set("sc_is_visitor_unique","rx4629288.1550729958.D340E62FE57B4FF097095612EED92D8C.1.1.1.1.1.1.1.1.1")
 
+def setCookies(r):
+    print("set cookies")
+    cookie_jar.set("4bd54_c_stamp",r.cookies["4bd54_c_stamp"])
+    if r.cookies.get("4bd54_ol_offset"):
+        cookie_jar.set("4bd54_ol_offset",r.cookies["4bd54_ol_offset"])
+    if r.cookies.get("4bd54_ipstate"):
+        cookie_jar.set("4bd54_ipstate",r.cookies["4bd54_ipstate"])
+    if r.cookies.get("4bd54_readlog"):
+        cookie_jar.set("4bd54_readlog",r.cookies["4bd54_readlog"])
+    if r.cookies.get("4bd54_lastvisit"):
+        cookie_jar.set("4bd54_lastvisit",r.cookies["4bd54_lastvisit"])
+    if r.cookies.get("sc_is_visitor_unique"):
+        cookie_jar.set("sc_is_visitor_unique",r.cookies["sc_is_visitor_unique"])
+    if r.cookies.get("4bd54_lastpos"):
+        cookie_jar.set("4bd54_lastpos",r.cookies["4bd54_lastpos"])
+
 def requestUrl(pageurl):
     r = requests.get(pageurl,headers = headers,cookies = cookie_jar)
     if(len(r.text) < 21):
         time.sleep(1)
-        print("set cookies")
-        cookie_jar.set("4bd54_c_stamp",r.cookies["4bd54_c_stamp"])
-        if r.cookies.get("4bd54_ol_offset"):
-            cookie_jar.set("4bd54_ol_offset",r.cookies["4bd54_ol_offset"])
-        if r.cookies.get("4bd54_ipstate"):
-            cookie_jar.set("4bd54_ipstate",r.cookies["4bd54_ipstate"])
-        if r.cookies.get("4bd54_readlog"):
-            cookie_jar.set("4bd54_readlog",r.cookies["4bd54_readlog"])
-        if r.cookies.get("4bd54_lastvisit"):
-            cookie_jar.set("4bd54_lastvisit",r.cookies["4bd54_lastvisit"])
-        if r.cookies.get("sc_is_visitor_unique"):
-            cookie_jar.set("sc_is_visitor_unique",r.cookies["sc_is_visitor_unique"])
-
+        setCookies(r)
         r = requests.get(pageurl,headers = headers,cookies = cookie_jar)
         # print(r.text.encode('unicode_escape').decode('string_escape'))
     # return etree.HTML(r.text)
@@ -84,7 +88,7 @@ def requestListPages(pageurl):
             # eleFullUrl = "http://x771117.net/bbs/read.php?tid=1940188&fpage=5"
 
             #请求子页面  等待1秒
-            time.sleep(5)
+            time.sleep(1)
             eleHtml = etree.HTML(requestUrl(eleFullUrl).text) 
             # result = etree.tostring(eleHtml)
             # print(result.decode('utf-8'))
@@ -101,12 +105,24 @@ def downloadImg(imgUrl,imgPath,referer,index=1):
     headers = {
         "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3626.400 QQBrowser/10.4.3211.400",
         "Referer": referer,
+        "Accept":"image/webp,image/apng,image/*,*/*;q=0.8",
+        "Accept-Encoding":"gzip, deflate",
+        "Accept-Language":"zh-CN,zh;q=0.9",
+        "Connection":"keep-alive",
+        "Host":"img1.imgfiles.info",
+        # "Cookie": "4bd54_ol_offset=166646; 4bd54_ipstate=1550729151; 4bd54_readlog=%2C1944825%2C; 4bd54_c_stamp=1553222087; 4bd54_lastpos=other; 4bd54_lastvisit=1%091553222087%09%2Fbbs%2Fhitcache.php%3Ftid1951796; sc_is_visitor_unique=rx4629288.1553222259.BF25414F54224FF7D85B1CAACD708BCF.1.1.1.1.1.1.1.1.1",
     }
-    r = requests.get(imgUrl,headers = headers)
-    imgName = imgUrl.split("/")[-1]
-    fileName = "{}/{}_{}".format(imgPath,index,imgName)
-    with open(fileName,'wb') as img:
-        img.write(r.content)
+    try:
+        r = requests.get(imgUrl,headers = headers)
+        imgName = imgUrl.split("/")[-1]
+        fileName = "{}/{}_{}".format(imgPath,index,imgName)
+        with open(fileName,'wb') as img:
+            img.write(r.content)
+    except Exception as e:
+        print(e)
+        setCookies(r)
+        downloadImg(imgUrl,imgPath,referer,index=1)
+    
 
 def main():
     for pageindex in range(1,6):
